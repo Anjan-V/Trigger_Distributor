62e4f05342d2e3d22a2388bec69cc5f9d5f4e8b8
Anjan V <anjanvasudevan2006@gmail.com>
2026-06-20 10:13:36 +0530

    8. Updation

--- src/components/LogoIcon.jsx
+++ src/components/LogoIcon.jsx
@@ -7,37 +7,37 @@ const LogoIcon = ({ className }) => {
       className={className} 
       xmlns="http://www.w3.org/2000/svg"
     >
-      {/* Outer Bottom Swoosh (Dark Teal) */}
+      {/* Outer Bottom Swoosh (Royal Blue) */}
       <path 
         d="M 30 190 Q 110 140 175 80 Q 110 150 30 190 Z" 
-        fill="#1C5E78" 
+        fill="#0052B4" 
       />
 
-      {/* Inner Bottom Swoosh (Grey) */}
+      {/* Inner Bottom Swoosh (Light Blue) */}
       <path 
         d="M 15 160 Q 60 135 90 120 Q 60 145 15 160 Z" 
-        fill="#9CA3AF" 
+        fill="#00A2ED" 
       />
 
-      {/* Main Dark Blue Body (Torso to Right Arm) */}
+      {/* Main Body (Torso to Right Arm - Royal Blue) */}
       <path 
         d="M 40 175 Q 110 130 155 60 Q 120 105 75 100 Q 100 130 40 175 Z" 
-        fill="#29277A" 
+        fill="#0052B4" 
       />
 
-      {/* Teal Left Swoosh (Left Arm/Chest) */}
+      {/* Left Swoosh (Left Arm/Chest - Light Blue) */}
       <path 
         d="M 0 110 Q 50 85 95 95 Q 115 110 75 130 Q 30 125 0 110 Z" 
-        fill="#0071A7" 
+        fill="#00A2ED" 
       />
 
-      {/* Head */}
-      <circle cx="110" cy="55" r="18" fill="#29277A" />
+      {/* Head (Royal Blue) */}
+      <circle cx="110" cy="55" r="18" fill="#0052B4" />
 
-      {/* 8-Pointed Star */}
+      {/* 8-Pointed Star (Yellow) */}
       <path 
         d="M 165 15 L 168 25 L 178 25 L 170 31 L 173 41 L 165 35 L 157 41 L 160 31 L 152 25 L 162 25 Z" 
-        fill="#FBBF24" 
+        fill="#F9B612" 
       />
     </svg>
   );
